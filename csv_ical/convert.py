371800d39ce89b1b3d91da2fd06ad7612be59917
asmx1986 <sebastianm.alonso@gmail.com>
2019-01-29 22:49:52 -0300

    customizations to export calendar as timesheet

--- csv_ical/convert.py
+++ csv_ical/convert.py
@@ -8,6 +8,8 @@ from icalendar import Calendar, Event
 
 from typing import Dict, List  # NOQA
 
+from datetime import timedelta
+
 
 DEFAULT_CONFIG = {
     'HEADER_COLUMNS_TO_SKIP':  0,
@@ -16,8 +18,8 @@ DEFAULT_CONFIG = {
     'CSV_NAME': 0,
     'CSV_START_DATE': 1,
     'CSV_END_DATE': 1,
-    'CSV_DESCRIPTION': 2,
-    'CSV_LOCATION': 3,
+    # 'CSV_DESCRIPTION': 2,
+    'CSV_LOCATION': 2,
 }
 
 
@@ -38,7 +40,7 @@ class Convert():
 
     def read_ical(self, ical_file_location):  # type: (str) -> Calendar
         """ Read the ical file """
-        with open(ical_file_location, 'r') as ical_file:
+        with open(ical_file_location, 'r', encoding="utf8") as ical_file:
             data = ical_file.read()
         self.cal = Calendar.from_ical(data)
         return self.cal
@@ -47,7 +49,7 @@ class Convert():
         # type: (str, Dict[str, int]) -> List[List[str]]
         """ Read the csv file """
         csv_configs = self._generate_configs_from_default(csv_configs)
-        with open(csv_location, 'r') as csv_file:
+        with open(csv_location, 'r', encoding="utf8") as csv_file:
             csv_reader = csv.reader(csv_file)
             self.csv_data = list(csv_reader)
         self.csv_data = self.csv_data[csv_configs['HEADER_COLUMNS_TO_SKIP']:]
@@ -63,7 +65,7 @@ class Convert():
             event.add('summary', row[csv_configs['CSV_NAME']])
             event.add('dtstart', row[csv_configs['CSV_START_DATE']])
             event.add('dtend', row[csv_configs['CSV_END_DATE']])
-            event.add('description', row[csv_configs['CSV_DESCRIPTION']])
+            # event.add('description', row[csv_configs['CSV_DESCRIPTION']])
             event.add('location', row[csv_configs['CSV_LOCATION']])
             self.cal.add_component(event)
         return self.cal
@@ -74,11 +76,13 @@ class Convert():
             if event.name != 'VEVENT':
                 continue
             row = [
-                event.get('SUMMARY'),
+                event.get('SUMMARY').replace(',', ''),
                 event.get('DTSTART').dt,
                 event.get('DTEND').dt,
-                event.get('DESCRIPTION'),
+                # event.get('DESCRIPTION'),
                 event.get('LOCATION'),
+                int((event.get('DTEND').dt - event.get('DTSTART').dt) / timedelta(minutes=1)),
+                event.get('DTSTART').dt.strftime('%d/%m/%Y')
             ]
             row = [str(x) for x in row]
             self.csv_data.append(row)
@@ -86,12 +90,12 @@ class Convert():
     def save_ical(self, ical_location):  # type: (str) -> None
         """ Save the calendar instance to a file """
         data = self.cal.to_ical()
-        with open(ical_location, 'w') as ical_file:
+        with open(ical_location, 'w', encoding="utf8", newline='') as ical_file:
             ical_file.write(data.decode('utf-8'))
 
     def save_csv(self, csv_location):  # type: (str) -> None
         """ Save the csv to a file """
-        with open(csv_location, 'w') as csv_handle:
+        with open(csv_location, 'w', encoding="utf8", newline='') as csv_handle:
             writer = csv.writer(csv_handle)
             for row in self.csv_data:
                 writer.writerow(row)
